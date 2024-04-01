635dd19613f7d9a117f8db304c54f51e336cacce
Ezio <e@e416.dev>
2024-04-01 14:26:37 -0600

    sunrise race excursion

--- resize.py
+++ resize.py
@@ -11,12 +11,15 @@ def main() -> None:
         # print(root, dirs, files)
 
         for file in files:
+            if file == 'desktop.ini':
+                continue
+
             path: str = os.path.abspath(os.path.join(root, file))
 
             with Image.open(path) as image:
                 if image.size == (3840, 2160):
                     new = image.resize((1280, 720))
-                    new.save(os.path.abspath(os.path.join(root, file.replace('_', ''))))
+                    new.save(os.path.abspath(os.path.join(root, file)))
                     print(f'resized {path}')
 
 
